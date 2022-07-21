35a99a27ef1a9d0acf4c4fe5b110bab9fa715bda
Cocoatwix <cocoatwix@hotmail.com>
2022-07-21 15:50:45 -0300

    Added a "more" button to PC layout for collecting menu items which get hidden when the window is too small. More button displays a menu on click, but currently the menu is empty. Signs on navigation bar now move slightly when hovered. One image, no JS, all CSS.

--- script.js
+++ script.js
@@ -11,9 +11,12 @@ var sidebarClass;
 
 var sidebarTabClass;
 
-var navSignDivClass;
-
 var sidebarShown = false;
+
+var navSignDivClass;
+var moreSign;
+
+var moreMenu;
 
 
 function toggle_sidebar()
@@ -23,29 +26,12 @@ function toggle_sidebar()
 	{
 		sidebarDiv.style.animation = "slide_in 0.2s";
 		sidebarDiv.style.left      = "0px";
-		
-		//This allows me to automatically know how to bring
-		// hidden elements back
-		
-		/*
-		for (let element of sidebarClass)
-		{
-			if (element.classList.contains("blockStyle"))
-				element.style.display = "block";
-			else if (element.classList.contains("inlineblockStyle"))
-				element.style.display = "inline-block";
-			
-			else if (element.classList.contains("sidebarIcon") ||
-							 element.classList.contains("sidebarLabel"))
-				element.style.display = "inline-block";
-		}
-		*/
 	}
 	
 	else
 	{		
 		sidebarDiv.style.animation = "slide_out 0.2s";
-		sidebarDiv.style.left = "-250px";
+		sidebarDiv.style.left      = "-250px";
 	}
 
 	sidebarShown = !sidebarShown;
@@ -59,21 +45,46 @@ function inspect_signs()
 	let signCount = 0;
 	
 	//One sign every 200 pixels
-	//-50 is to give some breathing room (don't want scroll bar to show up)
+	//-30 is to give some breathing room (don't want scroll bar to show up)
 	let signCapacity = Math.floor((window.innerWidth - 30) / 200);
 	
 	console.log(signCapacity);
 	
+	//This hides signs when the screen is too small,
+	// as well as manages the "more" sign.
 	for (let sign of navSignDivClass)
 	{
-		if (signCount < signCapacity)
+		if (signCount < signCapacity-1)
+		{
 			sign.style.display = "block";
+		}
+		
+		else if (signCount < signCapacity)
+		{
+			sign.style.display = "none";
+			moreSign.style.display = "block";
+		}
 		
 		else
-			sign.style.display = "none";
+		{
+			if (sign.id != "moreSign")
+			{
+				sign.style.display = "none";
+			}
+		}
 		
 		signCount += 1;
 	}
+	
+	//Hide more arrow when not needed
+	if (signCapacity >= signCount-1)
+	{
+		moreSign.style.display = "none";
+		console.log("???");
+	}
+	
+	if (signCapacity == signCount-1)
+		navSignDivClass[signCapacity-1].style.display = "block";
 }
 
 
@@ -82,6 +93,9 @@ function check_layout()
 /** When viewport changes size, check to see if we need to make
 	  any adjustments to the layout. */
 {
+	//Always get rid of more menu when resiing the sceen
+	hide_more_menu()
+	
 	//This ensures the sidebar is properly closed when switching layouts
 	if (window.innerWidth > 800)
 	{
@@ -94,13 +108,43 @@ function check_layout()
 }
 
 
+function show_more_menu()
+/** Toggles the "more" menu for showing hidden options. */
+{
+	moreMenu.style.display = "block";
+	moreMenu.style.top = event.clientY + "px";
+	moreMenu.style.left = event.clientX + "px";
+	
+	//Have you ever seen code so elegant in your life?
+	setTimeout(function()
+	{
+		document.body.onclick = hide_more_menu;
+	}, 1);
+}
+
+
+function hide_more_menu()
+{
+	moreMenu.style.display = "none";
+	
+	setTimeout(function()
+	{
+		document.body.onclick = null;
+	}, 1);
+}
+
+
 function page_init()
-/** Runs when the page is fully loaded. */
+/** Runs when the page is fully loaded. Initialises DOM objects and
+    their references. */
 {
 	hamburger      = document.getElementById("hamburger");
 	sidebarDiv     = document.getElementById("sidebarDiv");
 	sidebarX       = document.getElementById("sidebarX");
 	sidebarXSpan   = document.getElementById("sidebarXSpan");
+	
+	moreSign = document.getElementById("moreSign");
+	moreMenu = document.getElementById("moreMenu");
 	
 	sidebarClass    = document.getElementsByClassName("sidebar");
 	sidebarTabClass = document.getElementsByClassName("sidebarTab");
@@ -108,6 +152,8 @@ function page_init()
 	
 	hamburger.onclick = toggle_sidebar;
 	sidebarX.onclick  = toggle_sidebar;
+	
+	moreSign.onclick  = show_more_menu;
 	
 	check_layout();
 }
